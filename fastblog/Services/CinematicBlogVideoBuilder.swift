bb89116f1fe047e606f235abcc7fe5be61e6ccfc
Justin Seo <carrier_mania@yahoo.com>
2026-05-03 03:04:46 -0700

    bloggo watermark placement

--- fastblog/Services/CinematicBlogVideoBuilder.swift
+++ fastblog/Services/CinematicBlogVideoBuilder.swift
@@ -618,7 +618,7 @@ enum CinematicBlogVideoBuilder {
         let chipW = chipPadH * 2 + iconBlockW + iconGap + innerTextW
         let chipH = chipPadV * 2 + max(iconSide, textRowH)
         let chipRect = CGRect(
-            x: w - margin - chipW - 65,
+            x: w - margin - chipW - 95,
             y: h - margin - chipH,
             width: chipW,
             height: chipH
